ddb59d6dafe7419b0f377761f776a60d5cbaa40a
Madson Araujo <madsonra@dcc.ufba.br>
2015-09-07 11:03:48 -0300

    novo arquivo global app

---
+++ svn/openwimesh/openflow-app/tags/0.1/global_ofctl_app.py
@@ -0,0 +1,18 @@
+import Pyro4
+import netifaces as ni
+
+class GreetingMaker(object):
+    def get_fortune(self, name):
+        return "Hello, {0}. Here is your fortune message:\n" \
+               "Behold the warranty -- the bold print giveth and the fine print taketh away.".format(name)
+
+
+host = ni.ifaddresses('ofsw0')[2][0]['addr']
+daemon = Pyro4.Daemon(host=host)                # make a Pyro daemon
+uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
+
+fo = open("/tmp/uri.txt","wb")
+fo.write(str(uri) )
+fo.close()
+#print uri      # print the uri so we can use it in the client later
+daemon.requestLoop()                   # start the event loop of the server to wait for calls
