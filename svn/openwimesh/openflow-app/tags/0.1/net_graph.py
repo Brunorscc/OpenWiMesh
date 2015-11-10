29c1ac69c40e70174a72acc2e10586e9465ab58b
Madson Araujo <madsonra@dcc.ufba.br>
2015-11-10 15:59:33 -0300

    other domain neigh

--- svn/openwimesh/openflow-app/tags/0.1/net_graph.py
+++ svn/openwimesh/openflow-app/tags/0.1/net_graph.py
@@ -58,8 +58,24 @@ class NetGraph(DiGraph, Controller, object):
         self.ofctl_list = {}
         self.route_ins_table = {}
         self.weight_selection_algorithm = None
+        self.other_domain_neigh = {}
         # init time stamp
         self.time_stamp = 0
+
+    def add_other_domain_neigh(self, my_sw,neigh, last_update=time()):
+        self.other_domain_neigh[my_sw] = []
+        self.other_domain_neigh[my_sw].append({'neigh': neigh, 'last_update': last_update })
+
+    def clear_outdated_od_neigh(self):
+        dead= []
+        for sw in self.other_domain_neigh:
+            for neigh in self.other_domain_neigh[sw]
+                if (time() - self.other_domain_neigh[sw][neigh][last_update]) > 20:
+                    dead.append({'sw' : sw , 'neigh' : neigh})
+
+        for n in dead:
+             del self.other_domain_neigh[ n['sw'] ] [n['neigh']]
+
 
     def add_route_ins(self, dst_ip, my_crossd_hw, dst_crossd_hw, last_update=time()):
         self.route_ins_table[dst_ip] = {'my_crossd_hw': my_crossd_hw, 'dst_crossd_hw': dst_crossd_hw, 'last_update': last_update }
@@ -302,6 +318,8 @@ class NetGraph(DiGraph, Controller, object):
             if neigh not in self.nodes() or self.node[neigh]['conn'] is None \
                     or self.node[neigh]['conn'].disconnected:
                 #print "neigh is %s" % neigh
+                self.add_other_domain_neigh(node,neigh)
+                self.clear_outdated_od_neigh()
                 continue
 
             residual_bw = d_speed_mbps
